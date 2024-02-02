2be671463a2eee37cce96fca33b9dcd07fb7d794
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-02-02 01:46:43 +0000

    Implemented Delete command

---
+++ XeShell/Commands/Impl/Delete.cs
@@ -0,0 +1,34 @@
+﻿using Spectre.Console;
+using XeSharp.Device;
+using XeSharp.Device.FileSystem;
+
+namespace XeShell.Commands.Impl
+{
+    [Command("del", "rm", Inputs = [ typeof(string) ])]
+    public class Delete : ICommand
+    {
+        public void Execute(List<Command> in_commands, Command in_command, XeDbgConsole in_console)
+        {
+            var path = (string)in_command.Inputs[0];
+
+            var node = in_console.FileSystem.GetNodeFromPath(path) ??
+                in_console.FileSystem.GetDirectoryFromPath(path);
+
+            if (node == null)
+                return;
+
+            if (node.Type == EXeFileSystemNodeType.Directory)
+            {
+                if (!AnsiConsole.Confirm($@"{node}\*, Are you sure?"))
+                    return;
+            }
+
+            node.Delete(in_console);
+        }
+
+        public bool ExecuteRaw(string[] in_args, XeDbgConsole in_console)
+        {
+            return false;
+        }
+    }
+}
