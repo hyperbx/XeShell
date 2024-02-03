3ec459a07172a01693ed28bdd9b76fd87bf93f92
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-02-03 03:11:22 +0000

    Implemented Download command

--- XeShell/Commands/Impl/Download.cs
+++ XeShell/Commands/Impl/Download.cs
@@ -1,4 +1,7 @@
-﻿using XeSharp.Device;
+﻿using Spectre.Console;
+using XeSharp.Device;
+using XeSharp.Helpers;
+using XeShell.Helpers;
 
 namespace XeShell.Commands.Impl
 {
@@ -9,6 +12,40 @@ namespace XeShell.Commands.Impl
         {
             var serverPath = (string)in_command.Inputs[0];
             var localPath = (string)in_command.Inputs[1];
+
+            var node = in_console.FileSystem.GetNodeFromPath(serverPath) ??
+                in_console.FileSystem.GetDirectoryFromPath(serverPath);
+
+            var fileCount = node.GetTotalNodes(true, false);
+            var fileIndex = 0;
+
+            var status = "Downloading file" + (fileCount == 0 ? "..." : "s...");
+
+            ConsoleHelper.ProgressCommon
+            (
+                ctx =>
+                {
+                    var task = ctx.AddTask(status);
+
+                    in_console.Client.ReadEvent += (s, e) =>
+                    {
+                        /* TODO: figure out why this sometimes surpasses the file count.
+                                 It seems to be related to passing directory paths to the
+                                 Download function and XBDM returning an empty bin response. */
+                        if (e.IsBegin)
+                            fileIndex = (int)Math.Min(fileCount, fileIndex + 1);
+
+                        task.Description(
+                            $"{status}" + (fileCount == 0 ? "" : $" ({fileIndex} / {fileCount}) ") +
+                            $"({FormatHelper.ByteLengthToDecimalString(e.BytesRead)} / {FormatHelper.ByteLengthToDecimalString(e.BytesTotal)})");
+
+                        task.MaxValue(e.BytesTotal);
+                        task.Value(e.BytesRead);
+                    };
+
+                    node.Download(localPath);
+                }
+            );
         }
 
         public bool ExecuteRaw(string[] in_args, XeDbgConsole in_console)
