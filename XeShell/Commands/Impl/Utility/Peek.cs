fc8a7e8a6fdc4ec11e713beb53ab8ad2b03647c6
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-02-14 21:36:06 +0000

    Peek: implemented peeking registers (that are likely to contain addresses)

--- XeShell/Commands/Impl/Utility/Peek.cs
+++ XeShell/Commands/Impl/Utility/Peek.cs
@@ -1,16 +1,46 @@
-﻿using XeSharp.Device;
+﻿using XeSharp.Debug;
+using XeSharp.Device;
 using XeSharp.Helpers;
 using XeSharp.Logger;
 
 namespace XeShell.Commands.Impl
 {
-    [Command("peek", Inputs = [ typeof(uint) ], OptionalInputs = [ typeof(uint) ])]
+    [Command("peek", Inputs = [ typeof(string) ], OptionalInputs = [ typeof(uint) ])]
     public class Peek : ICommand
     {
         public void Execute(List<Command> in_commands, Command in_command, XeConsole in_console)
         {
-            var addr = (uint)in_command.Inputs[0];
+            var source = (string)in_command.Inputs[0];
+
+            var addr = 0U;
             var len = 64U;
+
+            using (var debugger = new XeDebugger(in_console))
+            {
+                var register = source.ToLower();
+                var processor = debugger.GetProcessor();
+
+                switch (register)
+                {
+                    case "iar": addr = processor.IAR;       break;
+                    case "lr":  addr = processor.LR;        break;
+                    case "ctr": addr = (uint)processor.CTR; break;
+
+                    default:
+                    {
+                        if (register.StartsWith("gpr") && int.TryParse(register[3..], out int out_registerIndex))
+                        {
+                            addr = (uint)processor.GPR[out_registerIndex];
+                            break;
+                        }
+
+                        // Assume address.
+                        addr = MemoryHelper.ChangeType<uint>(source);
+
+                        break;
+                    }
+                }
+            }
 
             if (in_command.Inputs.Count > 1)
                 len = (uint)in_command.Inputs[1];
