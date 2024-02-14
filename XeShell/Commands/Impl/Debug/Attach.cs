a30cbc78e47d7901c96043cec7e0f9a1a3b61870
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-02-14 21:03:59 +0000

    Implemented Attach command

---
+++ XeShell/Commands/Impl/Debug/Attach.cs
@@ -0,0 +1,23 @@
+﻿using XeSharp.Debug;
+using XeSharp.Device;
+using XeSharp.Logger;
+
+namespace XeShell.Commands.Impl
+{
+    [Command("attach")]
+    public class Attach : ICommand
+    {
+        public void Execute(List<Command> in_commands, Command in_command, XeConsole in_console)
+        {
+            XeDebugger.Attach(in_console);
+            XeLogger.Log($"Debugger attached.");
+        }
+
+        public bool ExecuteRaw(string[] in_args, XeConsole in_console)
+        {
+            return false;
+        }
+
+        public void Dispose() { }
+    }
+}
