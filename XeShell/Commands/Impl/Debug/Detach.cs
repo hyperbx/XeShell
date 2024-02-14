1a94e5a98d085dffb020b5f87646c59449a6935d
Hyper <34012267+hyperbx@users.noreply.github.com>
2024-02-14 21:04:07 +0000

    Implemented Detach command

---
+++ XeShell/Commands/Impl/Debug/Detach.cs
@@ -0,0 +1,23 @@
+﻿using XeSharp.Debug;
+using XeSharp.Device;
+using XeSharp.Logger;
+
+namespace XeShell.Commands.Impl
+{
+    [Command("detach")]
+    public class Detach : ICommand
+    {
+        public void Execute(List<Command> in_commands, Command in_command, XeConsole in_console)
+        {
+            XeDebugger.Detach(in_console);
+            XeLogger.Log($"Debugger detached.");
+        }
+
+        public bool ExecuteRaw(string[] in_args, XeConsole in_console)
+        {
+            return false;
+        }
+
+        public void Dispose() { }
+    }
+}
